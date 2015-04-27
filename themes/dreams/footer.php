8b5a1408cde15f62fa1cbbefc510ff3c89677e77
Diego B. Correa <ddumst@gmail.com>
2015-04-26 23:40:21 -0500

    Contact Page - Need add script to send message

--- themes/dreams/footer.php
+++ themes/dreams/footer.php
@@ -1,11 +1,11 @@
 		<footer class="row">
-			<div class="info large-12 columns">
-				<div class="data large-6 columns">
-					<div class="icons"><span class="icon-phone"></span> (511) 462 7149</div>
-					<div class="icons"><span class="icon-mobile"></span> RPC: (51) 941 488 070 / (51) 994 841 239</div>
-					<div class="icons"><span class="icon-mail"></span> milagros@dreamseventos.pe / iveth@dreamseventos.pe</div>
+			<div class="info">
+				<div class="data small-12 large-6 columns">
+					<div class="icons"><span class="icon icon-phone"></span> <span class="text">(511) 462 7149</span></div>
+					<div class="icons"><span class="icon icon-mobile"></span> <span class="text">RPC: (51) 941 488 070 / (51) 994 841 239</span></div>
+					<div class="icons"><span class="icon icon-mail"></span> <span class="text">milagros@dreamseventos.pe / iveth@dreamseventos.pe</span></div>
 				</div>
-				<div class="follow large-6 columns">
+				<div class="follow small-12 large-6 columns">
 					<span>Síguenos en: </span>
 					<a href="#" class="fb-icon" target="_blank"></a>
 				</div>
--- themes/dreams/templates/template-contact.php
+++ themes/dreams/templates/template-contact.php
@@ -16,23 +16,26 @@
 
 			<div class="pagePost-content">
 				<form class="contact" action="">
-					<div class="field small-10 medium-10 large-centered large-10 columns">
-						<input type="text" class="small-10 medium-10 large-12 columns" placeholder="Nombre completo" required>
+					<div class="field small-12 medium-12 large-centered large-10 columns">
+						<input type="text" class="small-12 medium-12 large-12 columns" placeholder="Nombre completo" required>
 					</div>
-					<div class="field small-10 medium-10 large-centered large-10 columns">
-						<input type="email" class="small-10 medium-10 large-12 columns" placeholder="Correo eléctronico" required>
+					<div class="field small-12 medium-12 large-centered large-10 columns">
+						<input type="email" class="small-12 medium-12 large-12 columns" placeholder="Correo eléctronico" required>
 					</div>
-					<div class="field small-10 medium-10 large-centered large-10 columns">
-						<input type="text" class="small-10 medium-10 large-12 columns" placeholder="Asunto" required>
+					<div class="field small-12 medium-12 large-centered large-10 columns">
+						<input type="text" class="small-12 medium-12 large-12 columns" placeholder="Asunto" required>
 					</div>
-					<div class="field small-10 medium-10 large-centered large-10 columns">
-						<input type="tel" class="small-10 medium-10 large-12 columns" placeholder="# Teléfono Celular" required>
+					<div class="field small-12 medium-12 large-centered large-10 columns">
+						<input type="tel" class="small-12 medium-12 large-12 columns" placeholder="# Teléfono Celular" required>
 					</div>
-					<div class="field small-10 medium-10 large-centered large-10 columns">
-						<input type="tel" class="small-10 medium-10 large-12 columns" placeholder="# Teléfono Fijo" required>
+					<div class="field small-12 medium-12 large-centered large-10 columns">
+						<input type="tel" class="small-12 medium-12 large-12 columns" placeholder="# Teléfono Fijo" required>
 					</div>
-					<div class="field small-10 medium-10 large-centered large-10 columns">
-						<textarea class="small-10 medium-10 large-12 columns" placeholder="Mensaje" required></textarea>
+					<div class="field textarea small-12 medium-12 large-centered large-10 columns">
+						<textarea class="small-12 medium-12 large-12 columns" placeholder="Mensaje" required></textarea>
+					</div>
+					<div class="center submit-btn small-12 medium-12 large-12 columns">
+						<input type="submit" id="sendContact" class="small-12 medium-centered medium-4 large-centered large-3 columns" name="contactForm" value="Enviar mensaje">
 					</div>
 				</form>
 			</div>
